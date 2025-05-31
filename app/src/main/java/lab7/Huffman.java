e86037d94b5e2fad42abd8e811bd237583bc02e2
Rowen Stiles <stilese@wwu.edu>
2025-05-30 17:20:18 -0700

    Now builds the input string for printing later, charMap is added to the heap, and print statements in main; going to continue working to try to build the tree itself

--- app/src/main/java/lab7/Huffman.java
+++ app/src/main/java/lab7/Huffman.java
@@ -7,11 +7,14 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.HashMap;
-
+import heap.Heap;
+import java.lang.StringBuilder;
+//import avl.AVL;
 
 public class Huffman {
     HashMap<Character, Integer> charMap;
     Heap<Character,Integer> heap;
+    StringBuilder inputString = new StringBuilder("");
 
     public Huffman() {
         charMap = new HashMap<Character, Integer>();
@@ -31,9 +34,13 @@ public class Huffman {
 
         while (scanner.hasNext()) {
             String line = scanner.next();
+            inputString.append(line);
             for (char c : line.toCharArray())
                 charMap.merge(c, 1, (a, b) -> a + b); // if new addition, value is one; otherwise, value is previous plus one
         } scanner.close();
+
+        // add to heap for sorting, then can use poll() to get lowest values for building huffman.
+        charMap.forEach((key, value) -> heap.add(key, value));
     }
 
     public int getCharacterCount() {
@@ -46,5 +53,13 @@ public class Huffman {
 
     public static void main(String[] args) {
         Huffman huff = new Huffman(args[0]);
+
+        if(huff.inputString.length() < 100){
+            System.out.println("Input string: " + huff.inputString +
+            "\nEncoded string: " + "huff.encode()" +
+            "\nDecoded string: " + "huff.decode()");
+        }
+        System.out.println("\nDecoded equals input: " + "huff.equalCoding()" +
+            "\nCompression ratio: " + "length(encoded bitstring) / huff.charaCount / 8.0");
     }
 }
