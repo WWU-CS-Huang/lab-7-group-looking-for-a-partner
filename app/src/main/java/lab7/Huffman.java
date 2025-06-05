ede727aaa0b4eebee076c75edf57c075b703e2f6
Rowen Stiles <stilese@wwu.edu>
2025-06-04 20:16:00 -0700

    Unable to continue working, but added what was finished/attempted into the prints for whatever points you're willing to give. Was fun though!

--- app/src/main/java/lab7/Huffman.java
+++ app/src/main/java/lab7/Huffman.java
@@ -2,11 +2,6 @@
  * Rowen Stiles and Charles M., May 2025, Huffman Coding
  */
 
- /*
-  * Hey, I didn't expect to get a partner at all and was honestly just going to take the zero/low grade, but since
-  * you're here, I'll do my best to get an even amount of work done. You do seem to know coding much better than I
-  * do, so please feel free to change up things as you see fit. I'm rowenattack on Discord if you need to reach out!
-  */
 package lab7;
 
 import java.io.File;
@@ -19,12 +14,35 @@ import java.lang.StringBuilder;
 
 public class Huffman {
     HashMap<Character, Integer> charMap;
-    Heap<Character,Integer> heap;
+    Heap<Node,Integer> heap;
     StringBuilder inputString = new StringBuilder("");
+    public String traversal;
+
+    class Node {
+        Character key;
+        Integer value;
+        Node left;
+        Node right;
+
+        /** For the initial values, PRE combining. */
+        public Node(Character k, Integer v) {
+            key = k;
+            value = v;
+            left = null;
+            right = null;
+        }
+        /** For nodes that come during combining with null keys. */
+        public Node(Integer v, Node l, Node r) {
+            key = null;
+            value = v;
+            left = l;
+            right = r;
+        }
+    }
 
     public Huffman() {
         charMap = new HashMap<Character, Integer>();
-        heap = new Heap<Character,Integer>();
+        heap = new Heap<Node,Integer>();
     }
 
     public Huffman(String fileName) {
@@ -38,15 +56,44 @@ public class Huffman {
             return;
         }
 
-        while (scanner.hasNext()) {
-            String line = scanner.next();
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
             inputString.append(line);
             for (char c : line.toCharArray())
                 charMap.merge(c, 1, (a, b) -> a + b); // if new addition, value is one; otherwise, value is previous plus one
         } scanner.close();
 
-        // add to heap for sorting, then can use poll() to get lowest values for building huffman.
-        charMap.forEach((key, value) -> heap.add(key, value));
+        // turns the keyvalue pairs into nodes, then adds the node and value to the heap
+        charMap.forEach(
+            (key, value) -> {
+            Node n = new Node(key,value);
+            heap.add(n, value);
+        });
+
+        // Base case is that two are removed and only one is added back, so eventually the heap will only have the head node in it
+        while(heap.size() > 1){
+            Node firstPolledNode = heap.poll();
+            Node secondPolledNode = heap.poll();
+
+            Node connectingNode = new Node(firstPolledNode.value + secondPolledNode.value, firstPolledNode, secondPolledNode);
+            heap.add(connectingNode,connectingNode.value);
+        }
+    }
+
+    /** appends the values in the tree to String traversal using an in-order traversal.
+     * Borrowed from lab 3, edited slightly to work with this and be more readable.
+     * Just to prove that the tree is real and hopefully correct in printing. */
+    public void inOrder() {
+        traversal = "";
+        inOrder(heap.peek());
+    }
+    private void inOrder(Node n) {
+        if(n == null){
+            return;
+        }
+        inOrder(n.left);
+        traversal += n.key + ":" + n.value + ", ";
+        inOrder(n.right);
     }
 
     public int getCharacterCount() {
@@ -57,6 +104,8 @@ public class Huffman {
         return charMap.toString();
     }
 
+    
+
     public static void main(String[] args) {
         Huffman huff = new Huffman(args[0]);
 
@@ -66,6 +115,13 @@ public class Huffman {
             "\nDecoded string: " + "huff.decode()");
         }
         System.out.println("\nDecoded equals input: " + "huff.equalCoding()" +
-            "\nCompression ratio: " + "length(encoded bitstring) / huff.charaCount / 8.0");
+        "\nCompression ratio: " + "length(encoded bitstring) / huff.charaCount / 8.0");
+
+        /**Rowen Note: I'm personally unable to continue working due to many other assignments,
+         * so here's proof that unique characters are counted correctly and in-order traversal
+         * of what I had of the tree, at least! */
+        huff.inOrder();
+        System.out.println("\nTo prove unique characters: " + huff.charMap.toString());
+        System.out.println("To prove huffman tree is correct, in-order traversal: " + huff.traversal);
     }
 }
